92ffdf7380465b09cb80b2ce828c2bf1a2c7dcc0
Aidan <aidankmcalister@gmail.com>
2025-04-15 13:29:53 -0500

    init

---
+++ components/User.server.vue
@@ -0,0 +1,8 @@
+<script setup>
+import prisma from "~/lib/prisma";
+const user = await prisma.user.findFirst();
+</script>
+
+<template>
+  <p>{{ user?.name ?? "No user has been added yet." }}</p>
+</template>
